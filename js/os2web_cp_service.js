c6f7c6dced165adbe0efdfcd590f43f96f716404
Anders Bryrup <andersbryrup@gmail.com>
2013-02-25 13:30:04 +0100

    added qtip for tooltip

--- js/os2web_cp_service.js
+++ js/os2web_cp_service.js
@@ -1,2 +1,38 @@
 /* os2web_cp_service.js */
-console.log('os2web_cp_service loaded');
+jQuery(document).ready(function($) {
+	$( "#edit-from-date-value-datepicker-popup-0" ).datepicker({
+		showOn: "both",
+		buttonImage: "' . $calIcon . '",
+		buttonImageOnly: true,
+		dateFormat: "dd-mm-yy"
+	});
+	$( "#edit-to-date-value-datepicker-popup-0" ).datepicker({
+				showOn: "both",
+				buttonImage: "' . $calIcon . '",
+				buttonImageOnly: true,
+				dateFormat: "dd-mm-yy"
+	});
+	$( "a.qtip-this[title]" ).qtip({
+		style: {
+			padding: 1,
+			background: "#FFF",
+			color: "black",
+			textAlign: "center",
+			border: {
+				width: 1,
+				radius: 2,
+				color: "#DDD"
+			},
+			tip: "leftMiddle"
+		},
+		position: {
+			corner: {
+				target: "rightMiddle",
+				tooltip: "leftMiddle"
+			},
+			adjust: {
+				x : 10
+			}
+		}
+	});
+});
